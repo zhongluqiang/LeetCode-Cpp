344a36f957e587360fda1f7489978eecd2ab4bdd
zhongluqiang <zhong.rs232@gmail.com>
2020-11-07 09:44:23 +0800

    863.二叉树中所有距离为K的结点，递归解法

--- 863.二叉树中所有距离为-k-的结点/863.二叉树中所有距离为-k-的结点.cpp
+++ 863.二叉树中所有距离为-k-的结点/863.二叉树中所有距离为-k-的结点.cpp
@@ -20,6 +20,7 @@ using namespace std;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+#if 0
 class Solution {
     // 基于unordered_map构建无向图,key是当前结点,value是邻结点的集合，使用vector表示
     unordered_map<TreeNode *, vector<TreeNode*>> g;
@@ -32,7 +33,7 @@ class Solution {
         }
         if(child->left) buildGraph(child, child->left);
         if(child->right) buildGraph(child, child->right);
-    }
+    }   
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int K) {
         buildGraph(nullptr, root);      // 构建无向图
@@ -65,5 +66,65 @@ public:
         return ans;
     }
 };
+#else
+class Solution {
+    vector<int> ans;
+
+    // 收集与root结点距离为d的所有结点
+    void collect(TreeNode *root, int d) {
+        if(!root) {
+            return;
+        }
+        if(d == 0) {
+            ans.push_back(root->val);
+        }
+        collect(root->left, d-1);
+        collect(root->right, d-1);
+    }
+
+    // 递归收集root树中所有与target距离为k的结点，并返回与target的距离，如果距离为-1，表示target不在root树中
+    int distance(TreeNode *root, TreeNode *target, int k) {
+        if(!root) {
+            return -1;
+        }
+        if(root == target) {
+            collect(target, k);
+            return 0;
+        }
+
+        // 计算左右子树与target的距离
+        int l = distance(root->left, target, k);
+        int r = distance(root->right, target, k);
+
+        // target在root左子树中
+        if(l >= 0) {
+            // 左子树与target距离为k-1，说明根结点与target距离刚好为k
+            if(l == k-1) {
+                ans.push_back(root->val);
+            }
+            // 在右子树中收集与target距离为k-l-2的结点
+            collect(root->right, k-l-2);
+            return l + 1;
+        }
+
+        // target在root右子树中
+        if(r >= 0) {
+            if(r == k-1) {
+                ans.push_back(root->val);
+            }
+            collect(root->left, k-r-2);
+            return r + 1;
+        }
+        return -1;
+    } // end distance
+
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int K) {
+        ans.clear();
+        distance(root, target, K);
+        return ans;
+    } //end distanceK
+};
+#endif
 // @lc code=end
 
