da0d0d9f6e7d26274591a450a86736bfe5c9a083
zhongluqiang <zhong.rs232@gmail.com>
2020-12-24 20:58:54 +0800

    5.最长回文子串

---
+++ 5.最长回文子串/5.最长回文子串.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=5 lang=cpp
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+class Solution {
+    // 判断字符串s的[x, y]区间是否是回文的
+    static inline bool isPalindrome(string &s, int x, int y) {
+        while(x <= y) {
+            if(s[x] != s[y]) {
+                return false;
+            }
+            x++;
+            y--;
+        }
+        return true;
+    }
+
+public:
+    string longestPalindrome(string s) {
+        const int size = s.size();
+        for(int len = size; len >= 1; len--) {
+            for(int i = 0; i < size-len+1; i++) {
+                if(isPalindrome(s, i, i+len-1)) {
+                    return s.substr(i, len);
+                }
+            }
+        }
+        return "";
+    }
+};
+// @lc code=end
+
