4689c83e1ca71f832ddbaa1d8b97c196b1021101
zhongluqiang <zhong.rs232@gmail.com>
2020-12-29 15:48:02 +0800

    31.下一个排列

---
+++ 31.下一个排列/31.下一个排列.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=31 lang=cpp
+ *
+ * [31] 下一个排列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(), nums.end());
+    }
+};
+// @lc code=end
+
