2fb25c537b30570b6a5f24450965fdec0be0b893
zhongluqiang <zhong.rs232@gmail.com>
2021-01-07 15:00:54 +0800

    55.跳跃游戏

---
+++ 55.跳跃游戏/55.跳跃游戏.cpp
@@ -0,0 +1,28 @@
+#include "leetcode-definition.h"
+/*
+ * @lc app=leetcode.cn id=55 lang=cpp
+ *
+ * [55] 跳跃游戏
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        // 记录当前位置和当前能到达的最远位置
+        int cur_pos = 0, cur_max = nums[0]; 
+       
+        // just jump as you can still jump
+        while(cur_pos != nums.size()-1 && cur_pos < cur_max) {
+            // 往前跳一步，重新计算当前位置能跳到的最远位置
+            // 这里没限制跳跃的次数，那么一次跳一步和一次跳多步最终效果都是一样的
+            // 一次跳多步有可能略过了中间某些具有更优解的点，所以只跳一步即可
+            cur_pos++;
+            cur_max = max(cur_max, cur_pos+nums[cur_pos]);
+        }
+        // 返回是否跳到了终点
+        return cur_pos == nums.size() - 1;
+    }
+};
+// @lc code=end
+
