d0cfa7cef945e30af39ae62eb7909cd6bc33f2ec
zhongluqiang <zhong.rs232@gmail.com>
2021-01-15 15:45:38 +0800

    137.只出现一次的数字-ii

--- 137.只出现一次的数字-ii/137.只出现一次的数字-ii.cpp
+++ 137.只出现一次的数字-ii/137.只出现一次的数字-ii.cpp
@@ -7,18 +7,36 @@ using namespace std;
  */
 
 // @lc code=start
+#if 0 
+// 位运算+状态转移
+// 参考：https://leetcode-cn.com/problems/single-number-ii/solution/zi-dong-ji-wei-yun-suan-zui-xiang-xi-de-tui-dao-gu/
 class Solution {
 public:
-    // 用a,b来共同记录一个数出现3次以内的所有状态，初始时a,b都为0，表示这个数没出现过
-    // 当出现一次时，用a来表示，当出现两次时，用b来表示，当出现3次时，a,b又回到0
     int singleNumber(vector<int>& nums) {
-        int a = 0, b = 0;
-        for(auto num : nums) {
-            a = (a ^ num) & ~b;
-            b = (b ^ num) & ~a;
+        int once = 0, twice = 0;
+        for(auto x : nums) {
+            once = (once ^ x) & (~twice);
+            twice = (twice ^x) & (~once); 
         }
-        return a;
+        return once;
     }
 };
+#else 
+// 统计每个bit位出现次数
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0; i < 32; i++) {
+            int cnt = 0;
+            for(auto x : nums) {
+                cnt += (x >> i) & 0x1;
+            }
+            ans |= (cnt % 3) << i;  // cnt%3只有可能是0或1
+        }
+        return ans;
+    }
+};
+#endif
 // @lc code=end
 
